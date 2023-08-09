40f5f14efa771f1f203bc34b1295173eb31e8ac4
NymiKo <dimon.kabernik8@gmail.com>
2023-08-09 19:46:25 +0500

    Basefragment created

--- app/src/main/java/com/example/honkaihelper/fragments/HeroesListFragment.kt
+++ app/src/main/java/com/example/honkaihelper/fragments/HeroesListFragment.kt
@@ -1,19 +1,9 @@
 package com.example.honkaihelper.fragments
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.example.honkaihelper.R
+import com.example.honkaihelper.databinding.FragmentHeroesListBinding
 
-class HeroesListFragment : Fragment() {
+class HeroesListFragment :
+    BaseFragment<FragmentHeroesListBinding>(FragmentHeroesListBinding::inflate) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_heroes_list, container, false)
-    }
+
 }
