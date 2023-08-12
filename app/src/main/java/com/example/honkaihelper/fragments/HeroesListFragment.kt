96d953916306449eed7b4ffa1f011be6c1344347
NymiKo <dimon.kabernik8@gmail.com>
2023-08-12 18:08:54 +0500

    setting up the recycler view in a fragment

--- app/src/main/java/com/example/honkaihelper/fragments/HeroesListFragment.kt
+++ app/src/main/java/com/example/honkaihelper/fragments/HeroesListFragment.kt
@@ -2,14 +2,17 @@ package com.example.honkaihelper.fragments
 
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.honkaihelper.adapters.HeroesListAdapter
+import com.example.honkaihelper.adapters.HeroesListRecyclerViewAdapter
 import com.example.honkaihelper.databinding.FragmentHeroesListBinding
 import com.example.honkaihelper.models.Hero
 import com.example.honkaihelper.utils.ZoomOutPageTransformer
 
 class HeroesListFragment : BaseFragment<FragmentHeroesListBinding>(FragmentHeroesListBinding::inflate) {
 
-    private lateinit var mAdapter: HeroesListAdapter
+    private lateinit var mAdapterViewPager: HeroesListAdapter
+    private lateinit var mAdapterRecyclerView: HeroesListRecyclerViewAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -17,16 +20,17 @@ class HeroesListFragment : BaseFragment<FragmentHeroesListBinding>(FragmentHeroe
     }
 
     private fun setupView() {
-        setupViewPager()
+        //setupViewPager()
+        setupRecyclerView()
     }
 
     private fun setupViewPager() {
-        mAdapter = HeroesListAdapter()
+        mAdapterViewPager = HeroesListAdapter()
 
         binding.viewPagerHeroes.apply {
             setPageTransformer(ZoomOutPageTransformer())
 
-            adapter = mAdapter.apply {
+            adapter = mAdapterViewPager.apply {
                 mHeroesList = listOf(
                     Hero(0, "Вельт", "https://static.wikia.nocookie.net/honkai-star-rail/images/c/c3/Персонаж_Вельт_Сплэш-арт.png/revision/latest/scale-to-width-down/1000?cb=20230213151457&path-prefix=ru"),
                     Hero(1, "Цзинь Юань", "https://static.wikia.nocookie.net/houkai-star-rail/images/4/48/Character_Jing_Yuan_Splash_Art.png/revision/latest/scale-to-width-down/1000?cb=20230210115809"),
@@ -34,6 +38,17 @@ class HeroesListFragment : BaseFragment<FragmentHeroesListBinding>(FragmentHeroe
                 )
             }
         }
+    }
+
+    private fun setupRecyclerView() {
+        binding.recyclerViewHeroes.layoutManager = GridLayoutManager(requireContext(), 3)
+        mAdapterRecyclerView = HeroesListRecyclerViewAdapter()
+        mAdapterRecyclerView.mHeroesList = listOf(
+            Hero(0, "Вельт", "https://static.wikia.nocookie.net/honkai-star-rail/images/c/c3/Персонаж_Вельт_Сплэш-арт.png/revision/latest/scale-to-width-down/1000?cb=20230213151457&path-prefix=ru"),
+            Hero(1, "Цзинь Юань", "https://static.wikia.nocookie.net/houkai-star-rail/images/4/48/Character_Jing_Yuan_Splash_Art.png/revision/latest/scale-to-width-down/1000?cb=20230210115809"),
+            Hero(2, "Сервал", "https://static.wikia.nocookie.net/honkai-star-rail/images/5/52/Персонаж_Сервал_Сплэш-арт.png/revision/latest/scale-to-width-down/1000?cb=20230213151537&path-prefix=ru")
+        )
+        binding.recyclerViewHeroes.adapter = mAdapterRecyclerView
     }
 
     override fun onDestroyView() {
