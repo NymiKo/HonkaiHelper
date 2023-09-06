d895cd20949b587b6494d87484d7db650b137691
NymiKo <dimon.kabernik8@gmail.com>
2023-09-06 21:46:31 +0500

    Factory for AppComponent Added the Factory interface for AppComponent with getting context and changed getting AppComponent in the application class

--- app/src/main/java/com/example/honkaihelper/di/App.kt
+++ app/src/main/java/com/example/honkaihelper/di/App.kt
@@ -1,10 +1,15 @@
 package com.example.honkaihelper.di
 
 import android.app.Application
+import com.example.honkaihelper.di.components.AppComponent
 import com.example.honkaihelper.di.components.DaggerAppComponent
 
 class App: Application() {
 
-    val appComponent = DaggerAppComponent.create()
+    val appComponent: AppComponent by lazy { initializeComponent() }
+
+    private fun initializeComponent(): AppComponent {
+        return DaggerAppComponent.factory().create(applicationContext)
+    }
 
 }
--- app/src/main/java/com/example/honkaihelper/di/components/AppComponent.kt
+++ app/src/main/java/com/example/honkaihelper/di/components/AppComponent.kt
@@ -1,11 +1,17 @@
 package com.example.honkaihelper.di.components
 
+import android.content.Context
 import com.example.honkaihelper.activity.MainActivity
+import dagger.BindsInstance
 import dagger.Component
 
 @Component
 interface AppComponent {
 
+    @Component.Factory
+    interface Factory {
+        fun create(@BindsInstance applicationContext: Context): AppComponent
+    }
     fun inject(activity: MainActivity)
 
 }
