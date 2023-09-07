7dc8b403f4b65edc8bb7d1db608d12d72a6dc210
NymiKo <dimon.kabernik8@gmail.com>
2023-09-07 19:30:45 +0500

    Added ViewModelFactory

--- app/src/main/java/com/example/honkaihelper/di/ViewModelFactory.kt
+++ app/src/main/java/com/example/honkaihelper/di/ViewModelFactory.kt
@@ -7,3 +7,30 @@ import javax.inject.Inject
 import javax.inject.Provider
 import kotlin.reflect.KClass
 
+class ViewModelFactory @Inject constructor(
+    private val creators: @JvmSuppressWildcards Map<Class<out ViewModel>, Provider<ViewModel>>
+): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        var creator: Provider<out ViewModel>? = creators[modelClass]
+        if (creator == null) {
+            for ((key, value) in creators) {
+                if (modelClass.isAssignableFrom(key)) {
+                    creator = value
+                    break
+                }
+            }
+        }
+        if (creator == null) {
+            throw IllegalArgumentException("Unknown model class: $modelClass")
+        }
+        try {
+            @Suppress("UNCHECKED_CAST")
+            return creator.get() as T
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
+    }
+}
+
+@MapKey
+annotation class ViewModelKey(val value: KClass<out ViewModel>)
