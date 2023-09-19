6d22eeb8ce0a36b0b5a5567125951d5e02bab2c9
NymiKo <dimon.kabernik8@gmail.com>
2023-09-19 19:49:04 +0500

    created a layout for LoginFragment

---
+++ app/src/main/java/com/example/honkaihelper/login/LoginFragment.kt
@@ -0,0 +1,9 @@
+package com.example.honkaihelper.login
+
+import com.example.honkaihelper.databinding.FragmentLoginBinding
+import com.example.honkaihelper.fragments.BaseFragment
+
+class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::inflate) {
+
+
+}
