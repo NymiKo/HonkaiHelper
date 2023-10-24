6b6e77e2190d0f916ee6c033d51c69f20573fcee
NymiKo <dimon.kabernik8@gmail.com>
2023-10-24 16:07:40 +0500

    Added methods to get the hero's path from the server

--- app/src/main/java/com/example/honkaihelper/load_data/data/LoadDataService.kt
+++ app/src/main/java/com/example/honkaihelper/load_data/data/LoadDataService.kt
@@ -1,8 +1,18 @@
 package com.example.honkaihelper.load_data.data
 
+import com.example.honkaihelper.base_build_hero.data.model.Element
+import com.example.honkaihelper.base_build_hero.data.model.Path
+import com.example.honkaihelper.heroes.data.model.Hero
 import retrofit2.Response
 import retrofit2.http.GET
 
 interface LoadDataService {
+    @GET("/getHeroes.php")
+    suspend fun getHeroesList(): Response<List<Hero>>
 
+    @GET("/getPaths.php")
+    suspend fun getPathsList(): Response<List<Path>>
+
+    @GET("/getElements.php")
+    suspend fun getElementsList(): Response<List<Element>>
 }
