6a5f70c34487b3711e41de0bd00b7caa21fb1f0c
NymiKo <dimon.kabernik8@gmail.com>
2023-11-06 19:44:01 +0500

    Added an adapter for displaying hero decorations

--- app/src/main/java/com/example/honkaihelper/base_build_hero/adapters/RelicsAdapter.kt
+++ app/src/main/java/com/example/honkaihelper/base_build_hero/adapters/RelicsAdapter.kt
@@ -3,23 +3,24 @@ package com.example.honkaihelper.base_build_hero.adapters
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.example.honkaihelper.base.BaseAdapter
+import com.example.honkaihelper.databinding.ItemRelicBaseBuildHeroBinding
 import com.example.honkaihelper.databinding.ItemWeaponBaseBuildHeroBinding
 import com.example.honkaihelper.info_about_hero.data.model.Relic
 import com.example.honkaihelper.utils.gone
 import com.example.honkaihelper.utils.load
 
-class RelicsAdapter: BaseAdapter<ItemWeaponBaseBuildHeroBinding, Relic>() {
+class RelicsAdapter: BaseAdapter<ItemRelicBaseBuildHeroBinding, Relic>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val binding = ItemWeaponBaseBuildHeroBinding.inflate(layoutInflater, parent, false)
+        val binding = ItemRelicBaseBuildHeroBinding.inflate(layoutInflater, parent, false)
 
         return RelicsViewHolder(binding)
     }
 
-    private inner class RelicsViewHolder(private val binding: ItemWeaponBaseBuildHeroBinding): BaseViewHolder(binding) {
+    private inner class RelicsViewHolder(private val binding: ItemRelicBaseBuildHeroBinding): BaseViewHolder(binding) {
         override fun bind(model: Relic) {
-            binding.imageWeaponBaseBuildHero.load(model.image)
-            if (adapterPosition + 1 == list.size) binding.imageNextWeapon.gone()
+            binding.imageRelicBaseBuildHero.load(model.image)
+            if (adapterPosition + 1 == list.size) binding.imageNextRelic.gone()
         }
     }
 }
