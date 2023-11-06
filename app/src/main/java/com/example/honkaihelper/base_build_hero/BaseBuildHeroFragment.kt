1210e5d3bd5e972e2930f5f927b6638259f4abdc
NymiKo <dimon.kabernik8@gmail.com>
2023-11-06 17:55:44 +0500

    Added the recycler view and its setting for displaying relics

--- app/src/main/java/com/example/honkaihelper/base_build_hero/BaseBuildHeroFragment.kt
+++ app/src/main/java/com/example/honkaihelper/base_build_hero/BaseBuildHeroFragment.kt
@@ -4,19 +4,23 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.honkaihelper.base.BaseFragment
+import com.example.honkaihelper.base_build_hero.adapters.RelicsAdapter
 import com.example.honkaihelper.base_build_hero.adapters.WeaponsAdapter
 import com.example.honkaihelper.base_build_hero.data.model.Weapon
 import com.example.honkaihelper.databinding.FragmentBaseBuildHeroBinding
+import com.example.honkaihelper.info_about_hero.data.model.Relic
 
 class BaseBuildHeroFragment :
     BaseFragment<FragmentBaseBuildHeroBinding>(FragmentBaseBuildHeroBinding::inflate) {
 
     private lateinit var mAdapterWeapons: WeaponsAdapter
+    private lateinit var mAdapterRelic: RelicsAdapter
 
     override fun setupView() {
         setupToolbar()
         setupAdapters()
         setupWeaponRecyclerView()
+        setupRelicRecyclerView()
     }
 
     override fun uiStateHandle() {
@@ -31,6 +35,7 @@ class BaseBuildHeroFragment :
 
     private fun setupAdapters() {
         mAdapterWeapons = WeaponsAdapter()
+        mAdapterRelic = RelicsAdapter()
     }
 
     private fun setupWeaponRecyclerView() {
@@ -39,6 +44,14 @@ class BaseBuildHeroFragment :
             adapter = mAdapterWeapons
         }
         setListWeapon()
+    }
+
+    private fun setupRelicRecyclerView() {
+        binding.recyclerRelicBaseBuildHero.apply {
+            layoutManager = LinearLayoutManager(requireActivity(), RecyclerView.HORIZONTAL, false)
+            adapter = mAdapterRelic
+        }
+        setListRelic()
     }
 
     private fun setListWeapon() {
@@ -63,6 +76,39 @@ class BaseBuildHeroFragment :
                 "https://static.wikia.nocookie.net/honkai-star-rail/images/4/44/Световой_конус_Решимость_блестит_подобно_жемчужинам_пота_Карточка.png/revision/latest?cb=20230710214809&path-prefix=ru",
                 5,
                 1
+            )
+        )
+    }
+
+    private fun setListRelic() {
+        mAdapterRelic.list = listOf(
+            Relic(
+                1,
+                "Вестник, блуждающий в хакерском пространстве",
+                "Повышает скорость на 6%.",
+                "Повышает скорость всех союзников на 12%, когда владелец применяет сверхспособность к союзнику, на 1 ход. Эффект не складывается.",
+                "http://f0862137.xsph.ru/images/relics/Messenger_Traversing_Hackerspace.webp"
+            ),
+            Relic(
+                2,
+                "Головорез бандитской пустыни",
+                "Повышает мнимый урон на 10%.",
+                "Когда владелец наносит урон противнику с ослаблениями, его крит. шанс повышается на 10%, а когда владелец наносит урон противнику со статусом Заключение, его крит. урон повышается на 20%.",
+                "http://f0862137.xsph.ru/images/relics/Wastelander_of_Banditry_Desert.webp"
+            ),
+            Relic(
+                3,
+                "Вор падающего метеора",
+                "Повышает эффект пробития на 16%.",
+                "Повышает эффект пробития владельца на 16%. Когда владелец пробивает уязвимость противника, восстанавливает 3 ед. энергии.",
+                "http://f0862137.xsph.ru/images/relics/Thief_of_Shooting_Meteor.webp"
+            ),
+            Relic(
+                4,
+                "Орёл сумеречного рубежа",
+                "Повышает ветряной урон на 10%.",
+                "После использования сверхспособности действие владельца продвигается вперёд на 25%.",
+                "http://f0862137.xsph.ru/images/relics/Eagle_of_Twilight_Line.webp"
             )
         )
     }
