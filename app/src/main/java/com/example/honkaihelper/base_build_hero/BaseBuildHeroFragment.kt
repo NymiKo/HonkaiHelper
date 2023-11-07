9f51be21eadb628f65343b1cdea4da4687b3ed58
NymiKo <dimon.kabernik8@gmail.com>
2023-11-07 16:24:06 +0500

    Getting data and displaying it on the bill hero screen

--- app/src/main/java/com/example/honkaihelper/base_build_hero/BaseBuildHeroFragment.kt
+++ app/src/main/java/com/example/honkaihelper/base_build_hero/BaseBuildHeroFragment.kt
@@ -1,26 +1,43 @@
 package com.example.honkaihelper.base_build_hero
 
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import androidx.core.os.bundleOf
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.honkaihelper.App
 import com.example.honkaihelper.base.BaseFragment
 import com.example.honkaihelper.base_build_hero.adapters.DecorationsAdapter
 import com.example.honkaihelper.base_build_hero.adapters.RelicsAdapter
 import com.example.honkaihelper.base_build_hero.adapters.WeaponsAdapter
 import com.example.honkaihelper.base_build_hero.data.model.Weapon
 import com.example.honkaihelper.databinding.FragmentBaseBuildHeroBinding
+import com.example.honkaihelper.heroes.data.model.Hero
 import com.example.honkaihelper.info_about_hero.data.model.Decoration
 import com.example.honkaihelper.info_about_hero.data.model.Relic
 
 class BaseBuildHeroFragment :
     BaseFragment<FragmentBaseBuildHeroBinding>(FragmentBaseBuildHeroBinding::inflate) {
 
-    private val viewModel get() = viewModels<BaseBuildHeroViewModel> { viewModelFactory }
+    private val viewModel by viewModels<BaseBuildHeroViewModel> { viewModelFactory }
 
     private lateinit var mAdapterWeapons: WeaponsAdapter
     private lateinit var mAdapterRelics: RelicsAdapter
     private lateinit var mAdapterDecorations: DecorationsAdapter
+    private val idHero get() = requireArguments().getInt(ID_HERO)
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        (requireActivity().application as App).appComponent.baseBuildHeroComponent().create().inject(this)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel.getFullBaseBuildHero(idHero)
+    }
 
     override fun setupView() {
         setupToolbar()
@@ -31,7 +48,9 @@ class BaseBuildHeroFragment :
     }
 
     override fun uiStateHandle() {
-
+        viewModel.fullBaseBuildHero.observe(viewLifecycleOwner) {
+            mAdapterWeapons.list = it.weapons
+        }
     }
 
     private fun setupToolbar() {
@@ -51,7 +70,6 @@ class BaseBuildHeroFragment :
             layoutManager = LinearLayoutManager(requireActivity(), RecyclerView.HORIZONTAL, false)
             adapter = mAdapterWeapons
         }
-        setListWeapon()
     }
 
     private fun setupRelicRecyclerView() {
@@ -68,32 +86,6 @@ class BaseBuildHeroFragment :
             adapter = mAdapterDecorations
         }
         setListDecoration()
-    }
-
-    private fun setListWeapon() {
-        mAdapterWeapons.list = listOf(
-            Weapon(
-                1,
-                "Меч",
-                "https://static.wikia.nocookie.net/honkai-star-rail/images/3/3a/Световой_конус_Просто_надо_подождать_Карточка.png/revision/latest?cb=20230721121734&path-prefix=ru",
-                1,
-                1
-            ),
-            Weapon(
-                2,
-                "Момент победы",
-                "https://static.wikia.nocookie.net/honkai-star-rail/images/a/af/Световой_конус_Момент_победы_Карточка.png/revision/latest?cb=20230710214745&path-prefix=ru",
-                6,
-                2
-            ),
-            Weapon(
-                2,
-                "Решимость блестит подобно жемчужинам пота",
-                "https://static.wikia.nocookie.net/honkai-star-rail/images/4/44/Световой_конус_Решимость_блестит_подобно_жемчужинам_пота_Карточка.png/revision/latest?cb=20230710214809&path-prefix=ru",
-                5,
-                1
-            )
-        )
     }
 
     private fun setListRelic() {
@@ -157,5 +149,14 @@ class BaseBuildHeroFragment :
         binding.recyclerRelicBaseBuildHero.adapter = null
         binding.recyclerDecorationBaseBuildHero.adapter = null
         super.onDestroyView()
+    }
+
+    companion object {
+        private const val ID_HERO = "id"
+
+        @JvmStatic
+        fun newInstance(idHero: Int): Bundle {
+            return bundleOf(ID_HERO to idHero)
+        }
     }
 }
