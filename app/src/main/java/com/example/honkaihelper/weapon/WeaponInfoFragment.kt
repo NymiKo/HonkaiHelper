2d3c79e4b14a66a94ca2c9fe7bf63dc2188b9ec4
NymiKo <dimon.kabernik8@gmail.com>
2023-11-09 20:34:01 +0500

    Displaying weapon data in the UI

--- app/src/main/java/com/example/honkaihelper/weapon/WeaponInfoFragment.kt
+++ app/src/main/java/com/example/honkaihelper/weapon/WeaponInfoFragment.kt
@@ -1,25 +1,63 @@
 package com.example.honkaihelper.weapon
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.honkaihelper.App
 import com.example.honkaihelper.R
 import com.example.honkaihelper.base.BaseFragment
 import com.example.honkaihelper.databinding.FragmentWeaponInfoBinding
+import com.example.honkaihelper.utils.load
+import com.example.honkaihelper.utils.loadImageWithoutScale
 
 class WeaponInfoFragment : BaseFragment<FragmentWeaponInfoBinding>(FragmentWeaponInfoBinding::inflate) {
 
     private val idWeapon get() = requireArguments().getInt(ID_WEAPON)
+    private val viewModel by viewModels<WeaponInfoViewModel> { viewModelFactory }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        (requireActivity().application as App).appComponent.weaponInfoComponent().create().inject(this)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel.getWeapon(idWeapon)
+    }
 
     override fun setupView() {
-
+        setupToolbar()
+        getWeapon()
     }
 
     override fun uiStateHandle() {
 
+    }
+
+    private fun setupToolbar() {
+        binding.toolbarInfoAboutWeapon.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+    private fun getWeapon() {
+        viewModel.weapon.observe(viewLifecycleOwner) {
+            binding.toolbarInfoAboutWeapon.title = it.name
+            binding.imageWeapon.loadImageWithoutScale(it.image)
+            val rarityImage = when(it.rarity) {
+                0 -> R.drawable.icon_3_stars
+                1 -> R.drawable.icon_4_stars
+                2 -> R.drawable.icon_5_stars
+                else -> R.drawable.icon_3_stars
+            }
+            binding.imageRarityWeapon.loadImageWithoutScale(rarityImage)
+        }
     }
 
     companion object {
