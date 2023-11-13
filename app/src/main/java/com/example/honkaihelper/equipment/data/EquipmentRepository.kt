11cbff2ee5bdc56f61d05b23e5b658df6c38d4b9
NymiKo <dimon.kabernik8@gmail.com>
2023-11-13 23:18:49 +0500

    Data acquisition has been changed, instead of data from the server, data is read from the local database

--- app/src/main/java/com/example/honkaihelper/equipment/data/EquipmentRepository.kt
+++ app/src/main/java/com/example/honkaihelper/equipment/data/EquipmentRepository.kt
@@ -1,10 +1,9 @@
 package com.example.honkaihelper.equipment.data
 
-import com.example.honkaihelper.data.NetworkResult
 import com.example.honkaihelper.equipment.data.model.Equipment
 
 interface EquipmentRepository {
-    suspend fun getWeapon(path: Int): NetworkResult<List<Equipment>>
-    suspend fun getRelic(): NetworkResult<List<Equipment>>
-    suspend fun getDecoration(): NetworkResult<List<Equipment>>
+    suspend fun getWeapons(path: Int): List<Equipment>
+    suspend fun getRelics(): List<Equipment>
+    suspend fun getDecorations(): List<Equipment>
 }
--- app/src/main/java/com/example/honkaihelper/equipment/data/EquipmentRepositoryImpl.kt
+++ app/src/main/java/com/example/honkaihelper/equipment/data/EquipmentRepositoryImpl.kt
@@ -2,36 +2,35 @@ package com.example.honkaihelper.equipment.data
 
 import com.example.honkaihelper.data.NetworkResult
 import com.example.honkaihelper.data.handleApi
+import com.example.honkaihelper.data.local.dao.DecorationDao
+import com.example.honkaihelper.data.local.dao.RelicDao
+import com.example.honkaihelper.data.local.dao.WeaponDao
 import com.example.honkaihelper.equipment.data.model.Equipment
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class EquipmentRepositoryImpl @Inject constructor(
-    private val equipmentService: EquipmentService,
-    private val ioDispatcher: CoroutineDispatcher
+    private val ioDispatcher: CoroutineDispatcher,
+    private val weaponDao: WeaponDao,
+    private val relicDao: RelicDao,
+    private val decorationDao: DecorationDao
 ): EquipmentRepository {
-    override suspend fun getWeapon(path: Int): NetworkResult<List<Equipment>> {
-        return withContext(ioDispatcher) {
-            handleApi {
-                equipmentService.getWeapon(path)
-            }
+    override suspend fun getWeapons(path: Int): List<Equipment> = withContext(ioDispatcher) {
+        weaponDao.getWeaponByPath(path).map {
+            Equipment(it.idWeapon, it.image, it.rarity.toByte())
         }
     }
 
-    override suspend fun getRelic(): NetworkResult<List<Equipment>> {
-        return withContext(ioDispatcher) {
-            handleApi {
-                equipmentService.getRelic()
-            }
+    override suspend fun getRelics(): List<Equipment> = withContext(ioDispatcher) {
+        relicDao.getRelics().map {
+            Equipment(it.idRelic, it.image)
         }
     }
 
-    override suspend fun getDecoration(): NetworkResult<List<Equipment>> {
-        return withContext(ioDispatcher) {
-            handleApi {
-                equipmentService.getDecoration()
-            }
+    override suspend fun getDecorations(): List<Equipment> = withContext(ioDispatcher) {
+        decorationDao.getDecorations().map {
+            Equipment(it.idDecoration, it.image)
         }
     }
 }
