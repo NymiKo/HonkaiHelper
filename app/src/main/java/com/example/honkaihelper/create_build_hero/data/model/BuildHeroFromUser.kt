843d67a89ce1dfba91c66832d0ecd779d4565113
NymiKo <dimon.kabernik8@gmail.com>
2023-11-21 19:41:45 +0500

    A field for storing hero equipment stats has been added to the hero build data class

--- app/src/main/java/com/example/honkaihelper/create_build_hero/data/model/BuildHeroFromUser.kt
+++ app/src/main/java/com/example/honkaihelper/create_build_hero/data/model/BuildHeroFromUser.kt
@@ -4,5 +4,21 @@ data class BuildHeroFromUser(
     val idHero: Int,
     val idWeapon: Int,
     val idRelic: Int,
-    val idDecoration: Int
-)
+    val idDecoration: Int,
+    val statsEquipment: Array<String>
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as BuildHeroFromUser
+
+        if (!statsEquipment.contentEquals(other.statsEquipment)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return statsEquipment.contentHashCode()
+    }
+}
