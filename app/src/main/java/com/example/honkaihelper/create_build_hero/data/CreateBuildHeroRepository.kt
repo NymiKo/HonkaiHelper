cd155bbcaa5b3a9a9a00d5e16715f9814544d0b9
NymiKo <dimon.kabernik8@gmail.com>
2023-11-21 19:38:57 +0500

    A method has been added to the repository to process the result of saving the hero build on the server

--- app/src/main/java/com/example/honkaihelper/create_build_hero/data/CreateBuildHeroRepository.kt
+++ app/src/main/java/com/example/honkaihelper/create_build_hero/data/CreateBuildHeroRepository.kt
@@ -1,10 +1,11 @@
 package com.example.honkaihelper.create_build_hero.data
 
 import com.example.honkaihelper.create_build_hero.data.model.BuildHeroFromUser
+import com.example.honkaihelper.data.NetworkResult
 import com.example.honkaihelper.data.local.models.hero.HeroWithNameAvatarRarity
 import com.example.honkaihelper.heroes.data.model.Hero
 
 interface CreateBuildHeroRepository {
     suspend fun getHero(idHero: Int): Hero
-    suspend fun saveBuild(buildHeroFromUser: BuildHeroFromUser)
+    suspend fun saveBuild(buildHeroFromUser: BuildHeroFromUser): NetworkResult<Boolean>
 }
--- app/src/main/java/com/example/honkaihelper/create_build_hero/data/CreateBuildHeroRepositoryImpl.kt
+++ app/src/main/java/com/example/honkaihelper/create_build_hero/data/CreateBuildHeroRepositoryImpl.kt
@@ -1,6 +1,8 @@
 package com.example.honkaihelper.create_build_hero.data
 
 import com.example.honkaihelper.create_build_hero.data.model.BuildHeroFromUser
+import com.example.honkaihelper.data.NetworkResult
+import com.example.honkaihelper.data.handleApi
 import com.example.honkaihelper.data.local.dao.HeroDao
 import com.example.honkaihelper.data.local.models.hero.HeroWithNameAvatarRarity
 import com.example.honkaihelper.heroes.data.model.Hero
@@ -10,13 +12,14 @@ import javax.inject.Inject
 
 class CreateBuildHeroRepositoryImpl @Inject constructor(
     private val ioDispatcher: CoroutineDispatcher,
-    private val heroDao: HeroDao
+    private val heroDao: HeroDao,
+    private val createBuildHeroService: CreateBuildHeroService
 ): CreateBuildHeroRepository {
     override suspend fun getHero(idHero: Int): Hero = withContext(ioDispatcher) {
         return@withContext heroDao.getHero(idHero).toHero()
     }
 
-    override suspend fun saveBuild(buildHeroFromUser: BuildHeroFromUser) {
-
+    override suspend fun saveBuild(buildHeroFromUser: BuildHeroFromUser): NetworkResult<Boolean> {
+        return handleApi { createBuildHeroService.saveBuild(buildHeroFromUser) }
     }
 }
