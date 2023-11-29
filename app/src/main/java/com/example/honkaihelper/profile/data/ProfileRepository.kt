81b0d469f845e0078edb7b54723442a854419007
NymiKo <dimon.kabernik8@gmail.com>
2023-11-29 13:15:36 +0500

    Getting and converting user data from the server response type

--- app/src/main/java/com/example/honkaihelper/profile/data/ProfileRepository.kt
+++ app/src/main/java/com/example/honkaihelper/profile/data/ProfileRepository.kt
@@ -2,6 +2,7 @@ package com.example.honkaihelper.profile.data
 
 import com.example.honkaihelper.data.NetworkResult
 import com.example.honkaihelper.profile.data.model.User
+import com.example.honkaihelper.profile.data.model.UserResponse
 import java.io.File
 
 interface ProfileRepository {
