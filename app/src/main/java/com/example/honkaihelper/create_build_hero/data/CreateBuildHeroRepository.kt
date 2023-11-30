0fea20015e4f3826ce95d65bf9740068248b82bd
NymiKo <dimon.kabernik8@gmail.com>
2023-11-30 14:31:14 +0500

    Added getting and updating the hero build

--- app/src/main/java/com/example/honkaihelper/create_build_hero/data/CreateBuildHeroRepository.kt
+++ app/src/main/java/com/example/honkaihelper/create_build_hero/data/CreateBuildHeroRepository.kt
@@ -4,8 +4,10 @@ import com.example.honkaihelper.create_build_hero.data.model.BuildHeroFromUser
 import com.example.honkaihelper.data.NetworkResult
 import com.example.honkaihelper.data.local.models.hero.HeroWithNameAvatarRarity
 import com.example.honkaihelper.heroes.data.model.Hero
+import com.example.honkaihelper.viewing_users_build.data.model.FullBuildHeroFromUser
 
 interface CreateBuildHeroRepository {
     suspend fun getHero(idHero: Int): Hero
     suspend fun saveBuild(buildHeroFromUser: BuildHeroFromUser): NetworkResult<Boolean>
+    suspend fun getBuild(idBuild: Int): NetworkResult<FullBuildHeroFromUser>
 }
