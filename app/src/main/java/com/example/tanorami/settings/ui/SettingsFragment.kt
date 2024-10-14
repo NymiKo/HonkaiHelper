f3d15b682006782b5c03723eb0d7ec6bd111645c
NymiKo <dimon.kabernik8@gmail.com>
2024-10-14 21:45:46 +0500

    The settings screen has been moved to jetpack compose

--- app/src/main/java/com/example/tanorami/settings/ui/SettingsFragment.kt
+++ app/src/main/java/com/example/tanorami/settings/ui/SettingsFragment.kt
@@ -1,8 +1,6 @@
 package com.example.tanorami.settings.ui
 
 import android.content.Context
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -54,73 +52,7 @@ class SettingsFragment : Fragment() {
         }
     }
 
-    fun setupView() {
-        setupToolbar()
-        setupButtonCheckUpdate()
-        setupButtonGoToFeedback()
-        setupButtonDonate()
-    }
-
-    fun uiStateHandle() {
-//        viewModel.uiState.observe(viewLifecycleOwner) {
-//            when(it) {
-//                is SettingsUIState.ERROR -> {
-//                    binding.buttonCheckUpdate.visible()
-//                    binding.progressCheckUpdate.gone()
-//                    toast(requireActivity(), it.message)
-//                }
-//                is SettingsUIState.LATEST_VERSION -> {
-//                    binding.buttonCheckUpdate.visible()
-//                    binding.progressCheckUpdate.gone()
-//                    toast(requireActivity(), R.string.already_have_latest_version_db)
-//                }
-//                is SettingsUIState.LOADING -> {
-//                    binding.buttonCheckUpdate.invisible()
-//                    binding.progressCheckUpdate.visible()
-//                }
-//                is SettingsUIState.NEW_VERSION -> {
-//                    toast(requireActivity(), R.string.discovered_new_version_db)
-//                    findNavController().navigate(R.id.loadDataFragment, LoadDataFragment.newInstance(it.versionDb))
-//                }
-//                is SettingsUIState.DATA_UPDATED -> {
-//                    setupTextVersionDb()
-//                }
-//            }
-//        }
-    }
-
-    private fun setupToolbar() {
-//        binding.toolbarSettings.setNavigationOnClickListener {
-//            findNavController().popBackStack()
-//        }
-    }
-
-    private fun setupTextVersionDb() {
-        //binding.textVersionDb.text = getString(R.string.version_db, getSharedPrefVersion().getString(
-        //    KEY_VERSION_DB, ""))
-    }
-
-    private fun setupButtonCheckUpdate() {
-//        binding.buttonCheckUpdate.setOnClickListener {
-//            getSharedPrefVersion().getString(KEY_VERSION_DB, "")?.let { version -> viewModel.checkUpdate(version) }
-//        }
-    }
-
-    private fun setupButtonGoToFeedback() {
-//        binding.buttonGoToFeedback.setOnClickListener {
-//            findNavController().navigate(R.id.sendFeedbackFragment)
-//        }
-    }
-
-    private fun setupButtonDonate() {
-//        binding.imageDonate.setOnClickListener {
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.donationalerts.com/r/nymiko"))
-            startActivity(intent)
-//        }
-    }
-
     companion object {
-        private const val KEY_VERSION_DB = "VERSION_DB"
         private const val ARG_DATA_UPLOADED = "path"
     }
 }
