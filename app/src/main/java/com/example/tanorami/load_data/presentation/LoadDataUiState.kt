ca340238dcf302cafe65537963c326c40de99075
NymiKo <dimon.kabernik8@gmail.com>
2024-10-29 18:31:59 +0500

    Deleted fragments from the project

--- app/src/main/java/com/example/tanorami/load_data/presentation/LoadDataUiState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.tanorami.load_data.presentation
-
-sealed class LoadDataUiState {
-    object LOADING: LoadDataUiState()
-    object SUCCESS: LoadDataUiState()
-    object ERROR: LoadDataUiState()
-}
